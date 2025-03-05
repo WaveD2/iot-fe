4910c38aedff60ead63faca73bf211b8b0b27f5f
đăng tùng <tungdev64@gmail.com>
2025-03-05 15:21:02 +0700

    update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HealthMonitorDashboard from '../components/HealthMonitorDashboard';
+import { listenToUserChannel } from '../websocket';
 
 export const DashboardPage: React.FC = () => {
   // Simulated user data
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
+  useEffect(() => {
+    if(!user) return
+    const cleanup = listenToUserChannel(user.id, (data) => {
+      alert(`chào bạn ${data.email}`);
+    });
+  
+    return cleanup;  
+  }, [user]);
+
   const userData = {
-    email: 'joib@gmail.com',
     heartRateData: [
       { timestamp: 1614556800000, value: 65 },
       { timestamp: 1614643200000, value: 70 },
@@ -25,7 +35,7 @@ export const DashboardPage: React.FC = () => {
     <div className="min-h-screen bg-gray-100">
       <div className="container mx-auto px-4 py-8">
         <HealthMonitorDashboard 
-          email={userData.email}
+          email={user?.email || "test@gmail.com"}
           heartRateData={userData.heartRateData}
           bodyTempData={userData.bodyTempData}
         />
