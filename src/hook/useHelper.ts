532c97ad684b54f4a3730b251e74a52bcb363fda
đăng tùng <tungdev64@gmail.com>
2025-04-28 13:55:53 +0700

    fix bug time

--- src/hook/useHelper.ts
+++ src/hook/useHelper.ts
@@ -1,29 +1,35 @@
-export const useHelper =  { 
-     groupByMinute : (data : any): any => {
-        const grouped = new Map();
-
-        data.forEach((entry: any) => {
-            const timeKey = new Date(entry.createdAt).toISOString().slice(0, 16);
-            if (!grouped.has(timeKey)) {
-                grouped.set(timeKey, { sum: {}, count: 0 });
-            }
-
-            const group = grouped.get(timeKey);
-            group.count += 1;
-
-            Object.keys(entry).forEach((key) => {
-                if (typeof entry[key] === "number") {
-                    group.sum[key] = (group.sum[key] || 0) + entry[key];
-                }
-            });
+export const useHelper = {
+    groupByMinute: (data: any): any => {
+      const grouped = new Map();
+  
+      data.forEach((entry: any) => {
+        const utcDate = new Date(entry.createdAt);
+        const timeKey = utcDate.toISOString().slice(0, 16); 
+  
+        if (!grouped.has(timeKey)) {
+          grouped.set(timeKey, { sum: {}, count: 0 });
+        }
+  
+        const group = grouped.get(timeKey);
+        group.count += 1;
+  
+        Object.keys(entry).forEach((key) => {
+          if (typeof entry[key] === "number") {
+            group.sum[key] = (group.sum[key] || 0) + entry[key];
+          }
         });
-
-        return Array.from(grouped.entries()).map(([key, { sum, count }]) => {
-            const averagedData = Object.keys(sum).reduce(
-                (acc, key) => ({ ...acc, [key]: sum[key] / count }),
-                {}
-            );
-            return { createdAt: new Date(key), ...averagedData };
-        });
+      });
+  
+      return Array.from(grouped.entries()).map(([key, { sum, count }]) => {
+        const averagedData = Object.keys(sum).reduce(
+          (acc, key) => ({ ...acc, [key]: sum[key] / count }),
+          {}
+        );
+  
+        const vietnamTime = new Date(new Date(key).getTime() + 7 * 60 * 60 * 1000);
+  
+        return { createdAt: vietnamTime, ...averagedData };
+      });
     }
-}
+  }
+  
